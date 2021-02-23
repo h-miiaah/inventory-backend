ea42cb6c4b824e1a518964adbafbad94b2a587e1
h-miiaah <haim.miiaah5@gmail.com>
2021-02-22 21:22:55 -0500

    added validation to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
     belongs_to :box
 
     validates :description, :style, :color, :size, :quantity, :image_url, :kind, presence: true
+    validates_inclusion_of :kind, :in => ['add', 'remove']
 end

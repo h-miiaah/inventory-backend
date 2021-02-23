3805dd414eb52c1d615e5968bdccb5091708de5b
h-miiaah <haim.miiaah5@gmail.com>
2021-02-22 21:13:18 -0500

    added validations to box model and item model.

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,3 +1,5 @@
 class Box < ApplicationRecord
     has_many :items
+
+    validates :name, presence: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
     belongs_to :box
+
+    validates :description, :style, :color, :size, :quantity, :image_url, :kind, presence: true
 end

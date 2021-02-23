5a2c441b8c0af24924d57a28d641ae1d133eb9f3
h-miiaah <haim.miiaah5@gmail.com>
2021-02-22 21:18:56 -0500

    migrated the db.

--- db/migrate/20210223020009_create_items.rb
+++ db/migrate/20210223020009_create_items.rb
@@ -1,13 +1,13 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.integer, :box_id
-      t.string, :description
-      t.string, :style
-      t.string, :color
-      t.string, :size
-      t.integer, :quantity
-      t.string, :image_url
+      t.integer :box_id
+      t.string :description
+      t.string :style
+      t.string :color
+      t.string :size
+      t.integer :quantity
+      t.string :image_url
       t.string :kind
 
       t.timestamps

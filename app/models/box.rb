9951f3dbbc4f8f44cb8251eb008c8d92a42614dc
h-miiaah <haim.miiaah5@gmail.com>
2021-02-23 20:07:39 -0500

    added update_box_amount instance method to the box model.

--- app/models/box.rb
+++ app/models/box.rb
@@ -2,4 +2,19 @@ class Box < ApplicationRecord
     has_many :items
 
     validates :name, presence: true
+
+    def update_box_amount(item)
+        if item.kind == 'add'
+            self.amount = self.amount + item.quantity
+            self.save
+        elsif item.kind == 'remove'
+            if self.amount >= item.quantity
+                self.amount = self.amount - item.quantity
+                self.save
+            else
+                return 'Quantity not enough.'
+            end
+        end
+    end
+
 end

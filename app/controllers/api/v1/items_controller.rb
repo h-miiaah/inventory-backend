b4bcd4d217bae9fc0e47821d2e54115f569e2235
h-miiaah <haim.miiaah5@gmail.com>
2021-02-23 20:58:46 -0500

    in items_controller added private set_box and item_params, index, create, show, destroy methods, and in routes nested the routes to make requests to the proper url format.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,3 +1,40 @@
 class Api::V1::ItemsController < ApplicationController
-    
+
+    before_action :set_box
+
+    def index
+        @items = @box.items
+        render json: @items
+    end
+
+    def create
+        @item = @box.items.new(item_params)
+        if @box.update_box_balance(@item) != 'Quantity not enough.'
+            @item.save
+            render json: @item
+        else
+            render json: {error: 'Quantity not enough.'} 
+        end
+    end
+
+    def show
+        @item = Item.find(params[:id])
+        # @item = @box.items.find_by(id: params[:id])
+        render json: @item
+    end
+
+    def destroy
+        @box.destroy     
+    end
+
+    private
+
+    def set_box
+        @box = Box.find(params[:box_id])
+    end
+
+    def item_params
+        params.require(:item).permit(:box_id, :description, :style, :color, :size, :quantity, :image_url, :kind)
+    end
+
 end

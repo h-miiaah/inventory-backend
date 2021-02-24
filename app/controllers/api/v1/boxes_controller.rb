3778c29fb0f70ef8c2633485becaa305a11be9e7
h-miiaah <haim.miiaah5@gmail.com>
2021-02-23 20:23:30 -0500

    in BoxesController added index, create, show, destroy, and private box_params.

--- app/controllers/api/v1/boxes_controller.rb
+++ app/controllers/api/v1/boxes_controller.rb
@@ -1,2 +1,33 @@
 class Api::V1::BoxesController < ApplicationController
+
+    def index
+        @boxes = Box.all
+        render json: @boxes
+    end
+
+    def create
+        @box = Box.new(box_params)
+        if @box.save
+            render json: @box
+        else
+            render json: {error: 'Unable to create box, please try again.'}
+        end
+    end
+
+    def show
+        @box = Box.find(params[:id])
+        render json: @box
+    end
+
+    def destroy
+        @box = Box.find(params[:id])
+        @box.destroy       
+    end
+
+    private
+
+    def box_params
+        params.require(:box).permit(:name, :amount)
+    end
+
 end

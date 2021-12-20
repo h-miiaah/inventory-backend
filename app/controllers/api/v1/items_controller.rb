8b2dd6bd8ed37a1f3ed77b31e6a75bdf8d5aa5fa
h-miiaah <haim.miiaah5@gmail.com>
2021-12-19 21:42:58 -0500

    added notes

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -46,3 +46,18 @@ class Api::V1::ItemsController < ApplicationController
     end
 
 end
+
+
+=begin
+
+- The code starts by declaring the class Api::V1::ItemsController < ApplicationController.
+- This is a controller that will be used to handle requests for items from the box.
+- The before_action :set_box tells Rails what code should run before any other actions in this controller are called.
+- In this case, it sets up an instance variable @items with all of the items in the box and then renders json: @items.
+- The index action returns an array of all of the items in the box, which can then be rendered on screen using JSON or XML depending on how you want to display them.
+- The create action will create a new item.
+- The show action will display the selected item.
+- The destroy action will delete the item requested to be deleted.
+- The code also contains before_action :set_box which will be executed before the index action, this will set the box for the item.
+
+=end

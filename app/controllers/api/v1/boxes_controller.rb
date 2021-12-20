911b4536aaef4426364bcad7075fff52c40706ae
h-miiaah <haim.miiaah5@gmail.com>
2021-12-19 21:36:50 -0500

    added notes

--- app/controllers/api/v1/boxes_controller.rb
+++ app/controllers/api/v1/boxes_controller.rb
@@ -38,3 +38,18 @@ class Api::V1::BoxesController < ApplicationController
     end
 
 end
+
+
+=begin
+
+- The code starts with a controller class called Api::V1::BoxesController.
+- This is the controller that will be used to handle all requests for Box objects.
+- The index action of this controller returns an array of all the boxes in the database
+- The create action creates a new box object using params passed in as arguments.
+- The update action updates an existing box by changing its name, and saves it back into the database.
+- The show action displays information about a specific box from our database.
+- The private method is where we define what parameters are required when creating or updating a Box object: name and amount .
+- The code will render a JSON response with the following:
+- { "boxes": [ { "name": "Box 1", "amount": 100 }, { "name": "Box 2", "amount": 200 } ] }
+
+=end

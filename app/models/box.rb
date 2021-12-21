7e0085daf97428417ddad5ef7ad0cda4e74b831e
h-miiaah <haim.miiaah5@gmail.com>
2021-12-20 22:39:52 -0500

    added noted box model

--- app/models/box.rb
+++ app/models/box.rb
@@ -32,3 +32,13 @@ class Box < ApplicationRecord
     end
 
 end
+
+=begin
+    
+1. Create a new class called Box that inherits from ApplicationRecord.
+2. Create a has_many association with items.
+3. Create a validates method that checks that the name is present.
+4. Create an update_box_amount method that updates the amount of the box.
+5. Create an update_box_amount_on_delete method that updates the amount of the box on delete.
+    
+=end

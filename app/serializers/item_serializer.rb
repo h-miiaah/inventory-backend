c33957b5c39af59e31b333762b031e7090db5542
h-miiaah <haim.miiaah5@gmail.com>
2021-12-24 00:00:42 -0500

    added notes

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,9 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :box_id, :description, :style, :color, :size, :quantity, :image_url, :kind
 end
+
+=begin
+  
+creates the serialization for the item objects and thier attributes.
+  
+=end

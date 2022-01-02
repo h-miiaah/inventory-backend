fac82c85e65ece3e51cfcaa35307d1056b022dc1
h-miiaah <haim.miiaah5@gmail.com>
2022-01-02 17:52:21 -0500

    application controller removed hoe method with render json message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
 
-    def home
-        render json: {message: "Server is up!"}
-    end
+    # def home
+    #     render json: {message: "Server is up!"}
+    # end
 
 end
